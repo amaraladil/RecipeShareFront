46d1441d2982d51e6ef827f04ad602cf84436241
Amar Al-Adil <amaraladil@hotmail.com>
2025-05-30 22:20:47 -0400

    feat: implement SEO metadata and head configuration for the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
+import { pageTitle } from '~/utils/meta'
 console.log('Index Page Loaded')
 const fetchApi = useApi()
 const recipes = ref([])
 
 onMounted(async () => {
   recipes.value = await fetchApi('/recipes/')
+})
+
+useSeoMeta({
+  title: pageTitle('Recent Recipes'),
+  description: 'Discover the latest recipes shared by our community.',
+  ogTitle: pageTitle('Recent Recipes'),
+  ogDescription: 'Explore the newest recipes from our users.',
+  ogImage: '/images/og-image.jpg',
+  ogUrl: useRuntimeConfig().public.baseUrl + '/',
+  twitterTitle: pageTitle('Recent Recipes'),
+  twitterDescription: 'Check out the latest recipes shared by our community.',
+  twitterImage: '/images/twitter-image.jpg',
+  twitterCard: 'summary_large_image'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
 })
 </script>
 
