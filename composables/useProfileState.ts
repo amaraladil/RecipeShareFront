4a4debc56cff50704c594082f4d1868740f88ec8
Amar Al-Adil <amaraladil@hotmail.com>
2026-01-19 11:48:56 -0500

    fix(ProfileState): cache author data when fetching current signed-in user

--- composables/useProfileState.ts
+++ composables/useProfileState.ts
@@ -3,10 +3,12 @@ export const useProfileState = () => useState('profile', () => null)
 export async function fetchCurrentUserProfile() {
   const api = useApi()
   const profileState = useProfileState()
+  const { cacheAuthor } = useAuthorCache()
   try {
     const data = await api('/users/me', { method: 'GET' })
     console.log('Fetched user profile:', data)
     profileState.value = data
+    cacheAuthor(data.id, data)
     return profileState.value
   } catch (e) {
     profileState.value = null
